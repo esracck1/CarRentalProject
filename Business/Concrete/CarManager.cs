f3d77a9e8f8f6e2d64861a1618dfbff2edd09158
esracck1 <esracck1@users.noreply.github.com>
2021-03-18 13:50:24 +0300

    17 Mart 2021

--- Business/Concrete/CarManager.cs
+++ Business/Concrete/CarManager.cs
@@ -3,6 +3,7 @@ using DataAccess.Abstract;
 using Entities.Concrete;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Business.Concrete
@@ -16,9 +17,44 @@ namespace Business.Concrete
             this.carDal = carDal;
         }
 
+        public void Add(Car car)
+        {
+            if (car.DailyPrice>0 && car.Description.Length>2)
+            {
+                carDal.Add(car);
+            }
+            else
+            {
+
+                Console.WriteLine("Car name should contain more than two characters.\n" +
+                                  "Car daily price should be more than '0'");
+            }
+           
+        }
+
+        public void Delete(Car car)
+        {
+            carDal.Delete(car);
+        }
+
         public List<Car> GetAll()
         {
            return carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return carDal.GetAll(c => c.BrandID == id).ToList();
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return carDal.GetAll(c => c.ColorID == id).ToList();
+        }
+
+        public void Update(Car car)
+        {
+            carDal.Update(car);
         }
     }
 }
---
+++ Business/Concrete/ColorManager.cs
@@ -0,0 +1,38 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Business.Concrete
+{
+    public class ColorManager : IColorService
+    {
+        IColorDal colorDal;
+        public ColorManager(IColorDal colorDal)
+        {
+            this.colorDal = colorDal;
+        }
+
+        public void Add(Color color)
+        {
+            colorDal.Add(color);
+        }
+
+        public void Delete(Color color)
+        {
+            colorDal.Delete(color);
+        }
+
+        public List<Color> GetAll()
+        {
+            return colorDal.GetAll();
+        }
+
+        public void Update(Color color)
+        {
+            colorDal.Update(color);
+        }
+    }
+}
